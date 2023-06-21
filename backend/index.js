0ef32937dbb9d21bd782125e45132d41dffc43c4
Amey Sawalkar <ameykishor@gmail.com>
2023-06-21 18:17:10 +0530

    frontend url added

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,8 @@ const port=process.env.PORT||5000
 
 
 const corsOptions = {
-  origin: "http://localhost:3000" // frontend URI (ReactJS)
+  // origin: "http://localhost:3000" // frontend URI (ReactJS)
+  origin: "https://visualization-dashboard-frontend.onrender.com" // frontend URI (ReactJS)
 }
 app.use(cors(corsOptions));
 app.use(express.json())
